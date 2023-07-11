32e86fb78ec9134ec7c21fda73df7acf4e69ff3a
weiminw <weimin.wan@deepgram.com>
2023-07-11 13:18:59 -0400

    allow user to pass url of audio

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { useState } from 'react';
-import Header from './Header'
+import Header from './components/Header/Header'
+import  UrlDialog from './components/Dialog/Dialog';
 import './Popup.css';
 import { Box, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
 
 const Popup = () => {
   const [transcript, setTranscript] = useState("This is transcript");
+
+  // URl buttons
+  const [openDialog, setOpenDialog] = useState(false);
+  const [url, setURl] = useState("");
+
+
+  const handleClickOpenUrl = () => {
+    setOpenDialog(true);
+  }
+
+  const handleClose = () => {
+    setOpenDialog(false);
+  }
+
+  const handleUrlChange = (e: any) => {
+    setURl(e.target.value);
+  }
 
   return (
     <div className="App">
@@ -13,24 +31,39 @@ const Popup = () => {
       <List>
         <ListItem>
           <ListItemButton>
-            <ListItemText primary="Record Audio" />
+            <ListItemText primary="Record Audio"/>
           </ListItemButton>
         </ListItem>
+
         <ListItem>
           <ListItemButton>
-            <ListItemText primary="Upload From URL" />
+            <ListItemText primary="Upload From URL" onClick={handleClickOpenUrl}/>
           </ListItemButton>
+          <UrlDialog open={openDialog} url={url} 
+          onClose={handleClose}
+          onUrlChange={handleUrlChange}
+          onTranscriptChange={setTranscript}
+          />
         </ListItem>
+
         <ListItem>
           <ListItemButton>
             <ListItemText primary="Upload Audio File" />
           </ListItemButton>
         </ListItem>
+
         <ListItem>
           <ListItemButton>
             <ListItemText primary="Capture From Tab" />
           </ListItemButton>
         </ListItem>
+
+        <ListItem>
+          <ListItemButton>
+            <ListItemText primary="Capture From Mic" />
+          </ListItemButton>
+        </ListItem>
+
       </List>
       <Box>
         <Typography> {transcript} </Typography>
