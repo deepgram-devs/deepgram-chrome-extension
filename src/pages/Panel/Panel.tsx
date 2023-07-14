916ec9422af39e1eea3641ba84f450ccdd4f67f1
weiminw <weimin.wan@deepgram.com>
2023-07-14 15:34:33 -0400

    change panel ui and guard ref

--- src/pages/Panel/Panel.tsx
+++ src/pages/Panel/Panel.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Button, TextareaAutosize, Stack } from '@mui/material';
+import { Stack, Button, TextField, Grid} from '@mui/material';
 import secret from '../../../secrets.development'
 import './Panel.css';
 
@@ -9,24 +9,13 @@ const Panel: React.FC = () => {
   const socketRef = useRef<WebSocket | null>(null);
   const recorderRef = useRef<MediaRecorder | null>(null);
 
-  // https://stackoverflow.com/a/47071576
-function mix(audioContext: AudioContext, streams: Array<MediaStream>) {
-  const dest = audioContext.createMediaStreamDestination()
-  streams.forEach(stream => {
-      const source = audioContext.createMediaStreamSource(stream)
-      source.connect(dest);
-  })
-  return dest.stream
-} 
-
   const handleStream = async () => {
     if (isStreaming) {
       setIsStreaming(false);
-      socketRef.current.close();
-      
+      if (socketRef.current) socketRef.current.close();
     } else {
       setIsStreaming(true);
-      socketRef.current = new WebSocket('wss://api.deepgram.com/v1/listen?model=general&tier=enhanced&smart_format=true&diarize=true', ['token', secret.APIKey])
+      socketRef.current = new WebSocket('wss://api.deepgram.com/v1/listen?diarize=true&smart_format=true', ['token', secret.APIKey])
       const screenStream = await(navigator.mediaDevices.getDisplayMedia({audio: true}));
       const micStream = await navigator.mediaDevices.getUserMedia({audio: true});
       const audioContext = new AudioContext();
@@ -37,8 +26,9 @@ function mix(audioContext: AudioContext, streams: Array<MediaStream>) {
         const data = JSON.parse(msg.data);
         console.log(data);
         if (!data.channel) {
-          socketRef.current.close();
-          recorderRef.current.stop();
+          // Server will send a metadata if it closes the connection. 
+          if (socketRef.current) socketRef.current.close();
+          if (recorderRef.current) recorderRef.current.stop();
           setIsStreaming(false);
           return;
         } else if (data.channel.alternatives[0].transcript) {
@@ -49,14 +39,15 @@ function mix(audioContext: AudioContext, streams: Array<MediaStream>) {
       });
 
       recorderRef.current.ondataavailable = (evt : any) => {
-        if (evt.data.size > 0 && socketRef.current.readyState == socketRef.current.OPEN) {
+        if (socketRef.current && evt.data.size > 0 
+          && socketRef.current.readyState == socketRef.current.OPEN) {
           console.log("data avaiable, sending through wss");
           socketRef.current.send(evt.data)
         }
       }
       
       socketRef.current.addEventListener('close', () => {
-        recorderRef.current.stop();
+        if (recorderRef.current) recorderRef.current.stop();
       });
 
       recorderRef.current.onstop = () => {
@@ -74,14 +65,41 @@ function mix(audioContext: AudioContext, streams: Array<MediaStream>) {
     }
     
   }
+
+  // https://stackoverflow.com/a/47071576
+  function mix(audioContext: AudioContext, streams: Array<MediaStream>) {
+    const dest = audioContext.createMediaStreamDestination()
+    streams.forEach(stream => {
+      const source = audioContext.createMediaStreamSource(stream)
+      source.connect(dest);
+    })
+    return dest.stream
+  } 
+
+  const handleClearText = () => {
+    setTranscript("");
+  }
+
   
   return (
-    <div className="container">
-      <Stack spacing={2}>
-      <Button onClick={handleStream}>{isStreaming ? "End Livestream" : "Start LiveStream"}</Button>
-      <TextareaAutosize minRows={30} maxLength={50} value={transcript}/>
+      <Stack direction={"column"} >
+        <Stack direction={"row"} justifyContent={"center"}> 
+          <Button 
+          onClick={handleStream}
+          >{isStreaming ? "End Livestream" : "Start LiveStream"}</Button>
+          <Button onClick={handleClearText}>Clear</Button>
+        </Stack>
+
+          <TextField 
+            multiline
+            variant='filled'
+            value={transcript} 
+            minRows={20} 
+            maxRows={Infinity} 
+            placeholder="Your Transcript Is Here">
+            
+            </TextField>
       </Stack>
-    </div>
   );
 };
 
